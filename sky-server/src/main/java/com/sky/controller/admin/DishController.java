29487824ee91e16cdc8da61562d8885afddedd78
c0ffeefish <qq1962401908@163.com>
2025-01-22 16:58:16 +0800

    新增分页查询菜品功能

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,16 +1,16 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -21,8 +21,16 @@ public class DishController {
     private DishService dishService;
 
     @PostMapping("")
+    @ApiOperation("新增菜品操作")
     public Result addDish(@RequestBody DishDTO dish) {
         dishService.addDish(dish);
         return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询")
+    public Result<PageResult> getDishPage(DishPageQueryDTO dishPageQueryDTO) {
+        PageResult pageResult = dishService.getDishPage(dishPageQueryDTO);
+        return Result.success(pageResult);
     }
 }
